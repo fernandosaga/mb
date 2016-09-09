178ac74a7e8757dede400ebce3d2d84e68f854e7
Fernando Saga <fernandosaga@gmail.com>
2016-09-08 23:49:08 -0300

    atualizando views/cliente/form.php

--- application/views/cliente/form.php
+++ application/views/cliente/form.php
@@ -49,8 +49,73 @@
 </div>
 
 <div class="form-group">
+    <?= form_label('Valor Cobrado:', 'vl_cobrado') ?>
+    <?= form_input(['name'=>'vl_cobrado', 'id'=>'vl_cobrado', 'class'=>'form-control', 'maxlength'=>255]) ?>
+</div>
+
+<div class="form-group">
+    <?= form_label('Valor Pago:', 'vl_pago') ?>
+    <?= form_input(['name'=>'vl_pago', 'id'=>'vl_pago', 'class'=>'form-control', 'maxlength'=>255]) ?>
+</div>
+
+<div class="form-group">
+    <?= form_label('Referência:', 'referencia') ?>
+    <?= form_input(['name'=>'referencia', 'id'=>'referencia', 'class'=>'form-control', 'maxlength'=>255]) ?>
+</div>
+
+<div class="form-group">
     <?= form_label('Observação:', 'observacao') ?>
     <?= form_textarea(['name'=>'observacao', 'id'=>'observacao', 'class'=>'form-control']) ?>
+</div>
+
+<div class="panel panel-default">
+    <div class="panel-heading"><strong>Telefones</strong></div>
+
+    <div class="panel-body">
+        <div class="form-group">
+            <?= form_label('Tipo:', 'tel_tipo') ?>
+            <?= form_dropdown(['name'=>'tel_tipo', 'id'=>'tel_tipo', 'class'=>'form-control'], [''=>'Selecione']); ?>
+        </div>
+
+        <div class="form-group">
+            <?= form_label('Número:', 'tel_numero') ?>
+            <?= form_input(['name'=>'telefone_numero', 'id'=>'telefone_numero', 'class'=>'form-control', 'maxlength'=>255]) ?>
+        </div>
+
+        <div class="form-group">
+            <?= form_label('Ramal:', 'tel_ramal') ?>
+            <?= form_input(['name'=>'tel_ramal', 'id'=>'tel_ramal', 'class'=>'form-control', 'maxlength'=>255]) ?>
+        </div>
+
+        <div class="form-group">
+            <?= form_label('Contato:', 'tel_contato') ?>
+            <?= form_input(['name'=>'tel_contato', 'id'=>'tel_contato', 'class'=>'form-control', 'maxlength'=>255]) ?>
+        </div>
+
+        <div class="form-group">
+            <?= form_label('departamento:', 'tel_departamento') ?>
+            <?= form_input(['name'=>'tel_departamento', 'id'=>'tel_departamento', 'class'=>'form-control', 'maxlength'=>255]) ?>
+        </div>
+    </div>
+</div>
+
+<div class="panel panel-default">
+    <div class="panel-heading"><strong>Setores</strong></div>
+
+    <div class="panel-body">
+
+        <div class="form-group">
+            <?= form_label('Nome do Setor:', 'set_descricao') ?>
+            <?= form_input(['name'=>'set_descricao', 'id'=>'set_descricao', 'class'=>'form-control', 'maxlength'=>255]) ?>
+        </div>
+
+        <div class="checkbox">
+            <label>
+                <input type="checkbox"> Principal
+            </label>
+        </div>
+
+    </div>
 </div>
 
 <div class="form-group">
